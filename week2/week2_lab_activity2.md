718241fa77b4e53a80ac566c8f4790668c1f451f
limmonjuice <zaimond.lim@lorma.edu>
2026-01-20 16:37:38 +0800

    completed activity 2

--- week2/week2_lab_activity2.md
+++ week2/week2_lab_activity2.md
@@ -3,3 +3,9 @@
 
 # 2
 ![](images/image2.png)
+
+# 3
+![](images/image3.png)
+
+# 4
+![](images/image4.png)
--- week2/week2_lab_activity2.sql
+++ week2/week2_lab_activity2.sql
@@ -1,5 +1,5 @@
 -- Activity 1
-CREATE OR REPLACE FUNCTION public.get_flight_duration(p_flight_id integer)
+CREATE OR REPLACE FUNCTION get_flight_duration(p_flight_id integer)
 RETURNS INTERVAL AS $$
 DECLARE
     v_flight_duration INTERVAL;
@@ -34,3 +34,47 @@ BEGIN
         END IF;
 END;
 $$ LANGUAGE plpgsql;
+
+-- Activity 3
+CREATE OR REPLACE PROCEDURE book_flight(
+    p_passenger_id INT,
+    p_flight_id INT,
+    p_seat_number VARCHAR
+) AS $$
+BEGIN
+	INSERT INTO bookings (
+	flight_id,
+	passenger_id,
+	booking_date,
+	seat_number,
+	status
+	)
+	VALUES(
+	p_flight_id,
+	p_passenger_id,
+	CURRENT_DATE,
+	p_seat_number,
+	'Confirmed'
+	);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Activity 4
+CREATE OR REPLACE PROCEDURE increase_prices_for_airline(
+	p_airline_id INT,
+	p_percentage_increase NUMERIC)
+AS $$
+DECLARE
+        v_flight RECORD;
+BEGIN
+        FOR
+                v_flight IN
+                SELECT flight_id, base_price FROM flights WHERE airline_id = p_airline_id
+        LOOP
+                UPDATE flights
+                SET base_price = base_price + (base_price * p_percentage_increase / 100)
+                WHERE flight_id = v_flight.flight_id;
+        END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
